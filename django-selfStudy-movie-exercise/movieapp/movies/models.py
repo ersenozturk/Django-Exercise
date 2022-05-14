fcd46cec3bef59b7a6cd424e75306b87f3ab17fb
ersen <70ozturkersen@gmail.com>
2022-05-14 23:35:41 +0300

    basic django project examined via youtube..instructor sadik turan's video.

---
+++ django-selfStudy-movie-exercise/movieapp/movies/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Movie(models.Model):
+    film_adi = models.CharField(max_length=200)
+    aciklama = models.TextField()
+    resim = models.CharField(max_length=100)
+    anasayfa = models.BooleanField(default=False)
