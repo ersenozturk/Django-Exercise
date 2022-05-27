7cfdd1b32601561a6b00e7bce32738ee3f12e7ab
ersen <70ozturkersen@gmail.com>
2022-05-27 13:23:22 +0300

    login operations added

--- session-10 Auth/authenticate/settings.py
+++ session-10 Auth/authenticate/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#!
+# login ouktan sonra redirect 'home' olsun istersek
+LOGIN_REDIRECT_URL = 'home'
--- session-10 Auth/authenticate/urls.py
+++ session-10 Auth/authenticate/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("user_example.urls")),
+    path('accounts/', include('django.contrib.auth.urls'))
+
 ]
