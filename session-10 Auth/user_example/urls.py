88e1f0212e6cf947d09869d1a3caf58bf36e624f
ersen <70ozturkersen@gmail.com>
2022-05-27 13:54:02 +0300

    register operations added

--- session-10 Auth/user_example/urls.py
+++ session-10 Auth/user_example/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_view, special
+from .views import home_view, special, register
 
 urlpatterns = [
     path('', home_view, name="home"),
     path('special', special, name="special"),
+    path('register', register, name="register"),
 ]
--- session-10 Auth/user_example/views.py
+++ session-10 Auth/user_example/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
 
 def home_view(request):    
     return render(request, "user_example/home.html")
@@ -6,3 +9,22 @@ def home_view(request):
 
 def special(request):    
     return render(request, "user_example/special.html")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username= username, password = password)
+            login(request, user)
+            return redirect('home')
+    
+    form = UserCreationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/register.html', context)
